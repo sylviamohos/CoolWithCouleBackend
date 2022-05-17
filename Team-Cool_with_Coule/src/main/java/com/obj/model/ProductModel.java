4e0f1e56ba13aa73d4aca2fb98478f720b3bffd9
mancorj <mr.jacksonator@gmail.com>
2022-05-17 15:06:51 -0600

    Created ProjectModel class

---
+++ Team-Cool_with_Coule/src/main/java/com/obj/model/ProductModel.java
@@ -0,0 +1,28 @@
+package main.java.com.obj.model;
+
+
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ProductModel {
+
+    private String name;
+
+    private String type;
+
+    private String upcCode;
+
+    private Integer quantity;
+
+    private String description;
+
+    private Long priceInCents;
+
+    private String imageUrl;
+
+}
