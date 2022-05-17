8121d0f4eecd0cd823a1bfd2fe42dee4d734a6e1
Ben Wilson <benwilson245@gmail.com>
2022-05-17 14:41:53 -0500

    commit

--- Team-Cool_with_Coule/src/main/java/com/obj/ResponseStatus.java
+++ Team-Cool_with_Coule/src/main/java/com/obj/ResponseStatus.java
@@ -1,4 +1,14 @@
 package main.java.com.obj;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class ResponseStatus {
+    private int code;
+    private String message;
 }
