5b457ca4f8308bf250c3708e61e302282a8ca9f9
Anthony Owyeong <littledude96@gmail.com>
2022-05-17 13:41:22 -0700

    fin order stuff

--- Team-Cool_with_Coule/src/main/java/com/obj/Order.java
+++ Team-Cool_with_Coule/src/main/java/com/obj/Order.java
@@ -1,4 +1,33 @@
 package main.java.com.obj;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+//import main.java.com.obj.dao.model.CustomerModel;
+
+import java.util.ArrayList;
+import java.util.Date;
+import lombok.*;
+
+@Setter
+
+@DynamoDBTable(tableName = "order")
 public class Order {
+    private String orderId;
+    //TODO
+    //private CustomerModel customerModel;
+    private ArrayList<String> productNames;
+    private Date orderDate;
+
+    @DynamoDBHashKey(attributeName = "orderId")
+    public String getOrderId() {return orderId;}
+
+    @DynamoDBAttribute(attributeName = "productNames")
+    public ArrayList<String> getProductNames() {return productNames;}
+
+    @DynamoDBAttribute(attributeName = "orderDate")
+    public Date getOrderDate() {return this.orderDate;}
+
+
 }
---
+++ Team-Cool_with_Coule/src/main/java/com/obj/dao/model/OrderModel.java
@@ -0,0 +1,71 @@
+package main.java.com.obj.dao.model;
+
+import java.util.List;
+import java.util.Date;
+import lombok.*;
+import main.java.com.obj.ResponseStatus;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class OrderModel {
+
+    private String orderId;
+    private List<String> productNames;
+    private Date orderDate;
+    private ResponseStatus responseStatus;
+    //TODO
+    // private CustomerModel customerModel;
+
+
+    public OrderModel(Builder builder) {
+        this.orderId = builder.orderId;
+        this.orderDate = builder.orderDate;
+        this.productNames = builder.productNames;
+
+        // TODO
+        //this.customerModel = builder.customerModel;
+    }
+
+    public static Builder builder() {return new Builder();}
+
+    public static final class Builder {
+        private String orderId;
+        private List<String> productNames;
+        private Date orderDate;
+        private ResponseStatus responseStatus;
+        // TODO CustomerModel customerModel;
+
+        public Builder withOrderId(String orderIdToUse) {
+            this.orderId = orderIdToUse;
+            return this;
+        }
+
+        public Builder withProductNames(List<String> productNamesToUse) {
+            this.productNames = productNamesToUse;
+            return this;
+        }
+
+        public Builder withDate(Date orderDateToUse) {
+            this.orderDate = orderDateToUse;
+            return this;
+        }
+
+        public Builder withResponseStatus(ResponseStatus responseStatusToUse) {
+            this.responseStatus = responseStatusToUse;
+            return this;
+        }
+
+        //TODO
+        /*
+        public Builder withCustomerModel(CustomerModel customerModelToUse) {
+            this.customerModel = customerModelToUse;
+        }
+        */
+
+        public OrderModel build() {return new OrderModel(this);}
+    }
+}
