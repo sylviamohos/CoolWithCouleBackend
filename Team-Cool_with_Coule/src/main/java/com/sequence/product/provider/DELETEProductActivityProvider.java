ab2b9c86f4d6c5e87545d6cb495d4f574180107a
MancorJ <mr.jacksonator@gmail.com>
2022-05-23 10:34:33 -0600

    Added null checks to product classes

--- Team-Cool_with_Coule/src/main/java/com/sequence/product/provider/DELETEProductActivityProvider.java
+++ Team-Cool_with_Coule/src/main/java/com/sequence/product/provider/DELETEProductActivityProvider.java
@@ -11,6 +11,9 @@ import main.java.com.sequence.product.request.DELETEProductRequest;
 import main.java.com.sequence.product.result.DELETEProductResult;
 import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
 
+import java.lang.reflect.Field;
+import java.security.InvalidParameterException;
+
 @NoArgsConstructor
 public class DELETEProductActivityProvider implements RequestHandler<DELETEProductRequest, DELETEProductResult> {
 
@@ -18,7 +21,13 @@ public class DELETEProductActivityProvider implements RequestHandler<DELETEProdu
     public DELETEProductResult handleRequest(DELETEProductRequest deleteProductRequest, Context context) {
         ServiceComponent dagger = DaggerServiceComponent.create();
         try {
+            if (deleteProductRequest.getName() == null) {
+                throw new NullPointerException();
+            }
             return dagger.provideDELETEProductActivity().handleRequest(deleteProductRequest, context);
+        } catch (NullPointerException e) {
+            ResponseStatus status = new ResponseStatus(400, "Product name cannot be null");
+            return new DELETEProductResult(null, status);
         } catch (ProductDoesNotExistException e) {
             ResponseStatus status = new ResponseStatus(400, "Product does not exist with the name " + deleteProductRequest.getName());
             return new DELETEProductResult(null, status);
